683d52f3f16f37e984fc0929d2f379328316c129
MOHAMED MUSTAK M <mohamedmustak9355@gmail.com>
2026-02-16 19:29:06 +0530

    Update adaptive_engine.py

--- backend/adaptive_engine.py
+++ backend/adaptive_engine.py
@@ -1,11 +1,8 @@
 import numpy as np
-
 def adjust_difficulty(data):
     if len(data) < 3:
         return 3
-
     recent = np.mean([correct for _, correct, _ in data[-3:]])
-
     if recent > 0.8:
         return 5
     elif recent < 0.4:
